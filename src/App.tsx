6f57698d69196725d426cea6709f71733400234e
ledemkam <ledemkam@gmail.com>
2025-01-13 13:22:11 +0100

    feat(remove-cart-buton): remove-cart-button modified

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import RemoveCartButton from "features/cart/add-cart-product/ui/RemoveCartButton
 const App = () => {
 
   const removeToCart = () => {
-    alert('remove cart')
+    alert('removes cart')
   }
   return <RemoveCartButton removeToCart={removeToCart} />
     
