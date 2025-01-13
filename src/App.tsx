190c0bcffd5df1bc7809d6c8197bb47d5c00e5c8
ledemkam <ledemkam@gmail.com>
2025-01-13 21:22:25 +0100

    feat(list-products): productItem-setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Header from "common/layout/Header"
+import ProductItem from "features/product/list-products/ui/ProductItem"
 
 const onSubmit = (search: string) => {
   console.log(search)
@@ -7,9 +8,19 @@ const onSubmit = (search: string) => {
 
 const cartcount = 10
 
+const product = {
+  name: 'Mobile phone',
+  picture: '',
+  price: 1000,
+ };
+ 
+
 const App = () => {
   return (
-    <Header onSubmit={onSubmit} cartcount={cartcount} />
+    <>
+      <Header onSubmit={onSubmit} cartcount={cartcount} />
+      <ProductItem product={product} />
+    </>
   )
 }
 export default App
