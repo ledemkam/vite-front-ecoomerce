0c449bef76b4cfd6d3f4e319c1ff030eac19be3b
rico kamdem <ledemkam@gmail.com>
2025-01-23 08:57:47 +0100

    chore(jest): test-unit-setup

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+export default {
+  testEnvironment: 'jsdom',
+  transform: {
+   '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+   '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+   '^.+\\.svg$': 'jest-transformer-svg',
+   '^common/(.*)$': '<rootDir>/src/common/$1',
+   '^features/(.*)$': '<rootDir>/src/features/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  };
+  
