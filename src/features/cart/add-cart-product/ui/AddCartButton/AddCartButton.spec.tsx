8f7e410d4de86da7fcce38bdc54853a00bf5a76e
rico kamdem <ledemkam@gmail.com>
2025-01-23 13:46:16 +0100

    test(AddCartButton): component AddCartButton final-test

--- src/features/cart/add-cart-product/ui/AddCartButton/AddCartButton.spec.tsx
+++ src/features/cart/add-cart-product/ui/AddCartButton/AddCartButton.spec.tsx
@@ -1,13 +1,23 @@
 import { render,screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import AddCartButton from './AddCartButton';
 
 const addToCart = jest.fn();
 
-describe('AddCartButton', () => {
-  it('should display a button named "in den Warenkorb"',
-    ()=>{
-       render(<AddCartButton addToCart={addToCart} />);
-       expect(
-        screen.getByRole('button', { name: 'in den Warenkorb' }),).toBeInTheDocument();
+describe('AddCartButton',  () => {
+  it('should display a button named "in den Warenkorb"', 
+     ()=>{
+       render(<AddCartButton addToCart={addToCart} />);   
+        expect(
+         screen.getByRole('button', { name: 'in den Warenkorb' }),).toBeInTheDocument();
+});
+
+it('should call addToCart when button is clicked', 
+  async ()=>{
+    const user = userEvent.setup();
+     render(<AddCartButton addToCart={addToCart} />);
+    await user.click(screen.getByRole('button', { name: 'in den Warenkorb' }));
+    expect(addToCart).toHaveBeenCalled();
+  
 });
 });
