9fb3748c71c604a2e9dc018be8469a5624657138
rico kamdem <ledemkam@gmail.com>
2025-01-24 12:12:57 +0100

    test(Product): component Product final-test

--- src/features/product/display-product/hooks/useProduct/useProduct.tsx
+++ src/features/product/display-product/hooks/useProduct/useProduct.tsx
@@ -1,27 +1,22 @@
-import { useMatch } from 'react-router-dom';
-import { getProduct } from '../../api/getProducts';
-import { useQuery , skipToken} from '@tanstack/react-query';
+import { skipToken, useQuery } from '@tanstack/react-query';
 import { ProductItemData } from 'features/product/list-products/types';
+import { useMatch } from 'react-router-dom';
+import { getProduct } from '../../api/getProduct';
 
 type UseProductReturn = {
-   product?: ProductItemData
+  product?: ProductItemData;
 };
 
-
 const useProduct = (): UseProductReturn => {
-    const matchProductPage = useMatch('/product/:id');
-    const productIdInURL = matchProductPage?.params.id
+  const matchProductPage = useMatch('/product/:id');
+  const productIdInURL = matchProductPage?.params.id;
 
-
-const query = useQuery({
+  const query = useQuery({
     queryKey: ['product', productIdInURL],
-    queryFn: productIdInURL ? () => getProduct(productIdInURL) :
-   skipToken,
-   });
-   return {
+    queryFn: productIdInURL ? () => getProduct(productIdInURL) : skipToken,
+  });
+  return {
     product: query.data,
-   };
-   
-
+  };
 };
 export default useProduct;
